af0cb69e83df249239fdecc1de6682cfb7a055f5
BarbaraDurs0 <128370377+BarbaraDurs0@users.noreply.github.com>
2023-03-20 14:29:06 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("scrivi unnumero: ");
+    scanf("%d", &a);
+    printf("scrivi un altro numero: ");
+    scanf("%d", &b);
+    printf("I numeri che hai scelto sono: %d e %d\n", a, b);
+}
