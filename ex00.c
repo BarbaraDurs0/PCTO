16ec1123799be0ce12aaac572dad2251443e4097
BarbaraDurs0 <128370377+BarbaraDurs0@users.noreply.github.com>
2023-03-20 12:35:48 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,14 @@
+ //creare una variabile di tipo float, inizializzarla ad un valore a vostra scelta
+ //stampare la variabile
+
+ #include <stdio.h>
+
+int main()
+{
+    float a = 17.3805;
+    char b = 'R';
+    char c = 'S'; 
+    int tumadre = 10;
+
+    printf("ho svolto la richiesta con la variabile %c e %c senza eliminare la variabile %f, ma non mi posso scordare di %d\n", b, c, a, tumadre);
+}
