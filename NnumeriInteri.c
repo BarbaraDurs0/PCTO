0efe89b1adfe3e3b722adff79991f12f54c0636c
BarbaraDurs0 <128370377+BarbaraDurs0@users.noreply.github.com>
2023-03-22 09:44:21 +0100

    Add files via upload

--- NnumeriInteri.c
+++ NnumeriInteri.c
@@ -3,10 +3,13 @@
 int main()
 {
     int x;
-    int y = 0;
+    int y;
     int z = 0;
+    int k;
+    int j;
     printf("inserisci un numero positivo: ");
     scanf("%d", &x);
+    k = x;
     while(x > 0)
     {
         printf("inserisci un numero: ");
@@ -14,5 +17,7 @@ int main()
         z = z + y;
         x = x -1;
     }
+    j = z / k;
     printf("La somma di tutti i numeri che hai inserito è: %d\n", z);
+    printf("La media è: %d", j);
 }
