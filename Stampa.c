cb29b1754a2790b26aea7c64b70a7e5cfd21be60
BarbaraDurs0 <128370377+BarbaraDurs0@users.noreply.github.com>
2023-03-23 12:34:56 +0100

    Add files via upload

---
+++ Stampa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void stampa(char *L)
+{
+    int i = 0;
+
+    while(L[i] != 0)
+    {
+        printf("%c\n", L[i]);
+        i = i + 1;
+    }
+}
+int main()
+{
+    char *L = "Napoli";
+
+    stampa (L);
+}
